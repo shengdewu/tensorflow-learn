3bad88d90dd5551cf68dc19d8f3abaf58700ec7f
boofish@bb.com <boofish@bb.com>
2019-09-20 17:01:22 +0800

    update

--- rnn/lstm_imp.py
+++ rnn/lstm_imp.py
@@ -10,16 +10,17 @@ class LSTM_IMPL(object):
         self.__time_step = 10
         self.__out_num = 1
         self.__hide_num = [15]
-        self.__batch_size = 1
+        self.__batch_size = 60
         self.__lstm_mode = LSTM(len(self.__feature_col), self.__time_step, self.__out_num, self.__hide_num,self.__batch_size)
         return
 
-    def excute(self, path):
+    def excute(self, path, train=True):
         data_parse = data_frame(path, self.__batch_size, self.__time_step, self.__feature_col, self.__label_col)
         log_configure.init_log(log_name='lstm', log_path='./lstm-log')
         optimize = gradient_descent(train_step=200, mode_path='./model/')
-        #self.__lstm_mode.train(data_parse.next_batch, optimize.generalization_optimize)
-
-        self.__lstm_mode.predict(data_parse.next_test, optimize.generalization_predict)
+        if train:
+            self.__lstm_mode.train(data_parse.next_batch, optimize.generalization_optimize)
+        else:
+            self.__lstm_mode.predict(data_parse.next_test, optimize.generalization_predict)
 
         return
